f687d54536184acea69f62b9babb6ac435e9b6f3
carlyle105142 <clychen@ucdavis.edu>
2022-06-16 16:04:45 -0700

    Initial commit

---
+++ Visualize.py
@@ -0,0 +1,11 @@
+from API import CovidData
+import streamlit as st
+from datetime import datetime
+
+cov_data = CovidData(desired_day=datetime(2020,9,11))
+df = cov_data.df
+
+col_names = ['Province_State', 'Confirmed', 'Deaths', 'Recovered', 'Active',
+             'Incident_Rate', 'Testing_Rate', 'Hospitalization_Rate', 'Mortality_Rate']
+
+st.table(df)
