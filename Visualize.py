5e47d7db43951ef23c847d10d5b12edd47080c55
carlyle105142 <clychen@ucdavis.edu>
2022-06-17 13:52:16 -0700

    some modification

--- Visualize.py
+++ Visualize.py
@@ -4,9 +4,11 @@ from datetime import datetime, timedelta
 
 date = datetime.today() - timedelta(days=1)
 cov_data = CovidData()
-df = cov_data.get_daily_data(date)
+# df = cov_data.get_daily_data(date)
 df2 = cov_data.get_cumulative_data()
 
 # date = st.date_input("Pick a date", value=datetime.today())
-st.dataframe(df2)
+st.area_chart(df2[df2['Province_State'] == 'California']['Confirmed'])
+
+states = df2.index.unique()
 
