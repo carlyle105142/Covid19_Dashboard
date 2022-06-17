f918b18ae2af7b1c40eee5c7718462e706d87ddb
carlyle105142 <clychen@ucdavis.edu>
2022-06-16 19:13:53 -0700

    Update Visualize.py

--- Visualize.py
+++ Visualize.py
@@ -8,4 +8,4 @@ df = cov_data.df
 col_names = ['Province_State', 'Confirmed', 'Deaths', 'Recovered', 'Active',
              'Incident_Rate', 'Testing_Rate', 'Hospitalization_Rate', 'Mortality_Rate']
 
-st.table(df)
+st.table(df.head(3))
