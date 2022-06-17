2cd575ad0684a9267878f04ea0e497757ab82ce5
carlyle105142 <clychen@ucdavis.edu>
2022-06-17 14:44:11 -0700

    some modification

--- Visualize.py
+++ Visualize.py
@@ -1,32 +1,15 @@
-# from data_interface import CovidData
-# import streamlit as st
-# from datetime import datetime, timedelta
+from data_interface import CovidData
+import streamlit as st
+from datetime import datetime, timedelta
 # import plotly.express as px
-#
-# date = datetime.today() - timedelta(days=1)
-# cov_data = CovidData()
-# df2 = cov_data.get_cumulative_data()
-#
+import matplotlib.pyplot as plt
+
+date = datetime.today() - timedelta(days=1)
+cov_data = CovidData()
+df2 = cov_data.get_cumulative_data()
+
+fig = plt.plot(df2[df2.Province_State == 'California']['Confirmed'])
+st.pyplot(fig)
+
 # fig1 = px.line(df2[df2.Province_State == 'California'], y='Confirmed')
 # st.plotly_chart(fig1)
-
-import streamlit as st
-import plotly.figure_factory as ff
-import numpy as np
-
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-         hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
