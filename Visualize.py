ab816336be7984575df6c0687ccf4ea89a9108a1
carlyle105142 <clychen@ucdavis.edu>
2022-06-17 14:20:15 -0700

    some modification

--- Visualize.py
+++ Visualize.py
@@ -10,7 +10,7 @@ df2 = cov_data.get_cumulative_data()
 
 # date = st.date_input("Pick a date", value=datetime.today())
 fig1 = px.line(df2[df2.Province_State == 'California'], x='date', y='Confirmed')
-fig1.show()
+st.plotly_chart(fig1, use_container_width=True)
 # st.line_chart(df2[df2.Province_State == 'California']['Confirmed'].diff())
 # st.line_chart(df2[df2.Province_State == 'California']['Deaths'].diff())
 # states = df2.index.unique()
