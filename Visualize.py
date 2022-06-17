bfbe8ca28a50f8bbb13c46f4549562bbf244ec86
carlyle105142 <clychen@ucdavis.edu>
2022-06-17 14:25:47 -0700

    some modification

--- Visualize.py
+++ Visualize.py
@@ -1,17 +1,32 @@
-from data_interface import CovidData
+# from data_interface import CovidData
+# import streamlit as st
+# from datetime import datetime, timedelta
+# import plotly.express as px
+#
+# date = datetime.today() - timedelta(days=1)
+# cov_data = CovidData()
+# df2 = cov_data.get_cumulative_data()
+#
+# fig1 = px.line(df2[df2.Province_State == 'California'], y='Confirmed')
+# st.plotly_chart(fig1)
+
 import streamlit as st
-from datetime import datetime, timedelta
-import plotly.express as px
+import plotly.figure_factory as ff
+import numpy as np
 
-date = datetime.today() - timedelta(days=1)
-cov_data = CovidData()
-# df = cov_data.get_daily_data(date)
-df2 = cov_data.get_cumulative_data()
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
-# date = st.date_input("Pick a date", value=datetime.today())
-fig1 = px.line(df2[df2.Province_State == 'California'], y='Confirmed')
-st.plotly_chart(fig1)
-# st.line_chart(df2[df2.Province_State == 'California']['Confirmed'].diff())
-# st.line_chart(df2[df2.Province_State == 'California']['Deaths'].diff())
-# states = df2.index.unique()
+# Group data together
+hist_data = [x1, x2, x3]
 
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+         hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
