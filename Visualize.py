d91a9f7c67f757d503d7660802061ff9f88f206c
carlyle105142 <clychen@ucdavis.edu>
2022-06-17 14:19:25 -0700

    some modification

--- Visualize.py
+++ Visualize.py
@@ -1,6 +1,7 @@
 from data_interface import CovidData
 import streamlit as st
 from datetime import datetime, timedelta
+import plotly.express as px
 
 date = datetime.today() - timedelta(days=1)
 cov_data = CovidData()
@@ -8,7 +9,9 @@ cov_data = CovidData()
 df2 = cov_data.get_cumulative_data()
 
 # date = st.date_input("Pick a date", value=datetime.today())
-st.line_chart(df2[df2.Province_State == 'California']['Confirmed'].diff())
-st.line_chart(df2[df2.Province_State == 'California']['Deaths'].diff())
+fig1 = px.line(df2[df2.Province_State == 'California'], x='date', y='Confirmed')
+fig1.show()
+# st.line_chart(df2[df2.Province_State == 'California']['Confirmed'].diff())
+# st.line_chart(df2[df2.Province_State == 'California']['Deaths'].diff())
 # states = df2.index.unique()
 
