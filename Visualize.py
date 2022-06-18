55e8b03488e6003872e7c33378a663efe1d8a933
carlyle105142 <clychen@ucdavis.edu>
2022-06-18 16:12:06 -0700

    some modification

--- Visualize.py
+++ Visualize.py
@@ -28,12 +28,14 @@ prev_state_df = prev_df[prev_df.Province_State == state]
 diff = str(state_df['Confirmed'][0] - prev_state_df['Confirmed'][0])
 death_diff = str(state_df['Deaths'][0] - prev_state_df['Deaths'][0])
 
-all_states_avg_mort = output_df['Mortality_Rate'].mean(axis=0)
+US_avg_mort = output_df['Mortality_Rate'].mean(axis=0)
 mort_rate_diff = str(
-    round(100*(state_df['Mortality_Rate'][0] - all_states_avg_mort), 2)
+    round(100*(state_df['Mortality_Rate'][0] - US_avg_mort), 2)
 )+"%"
+
+US_avg_incident = output_df['Incident_Rate'].mean(axis=0)
 incident_rate_diff = str(
-    int(state_df['Incident_Rate'][0] - prev_state_df['Incident_Rate'][0])
+    int(state_df['Incident_Rate'][0] - US_avg_incident)
 )
 
 # st.dataframe(output_df)
@@ -46,9 +48,10 @@ col1.metric(label='Confirmed', value=int(state_df['Confirmed'][0]),
 col2.metric(label='Deaths', value=int(state_df['Deaths'][0]),
             delta=death_diff,
             delta_color='inverse')
-col3.metric(label='Case-Fatality Ratio', value=round(100*state_df['Mortality_Rate'][0], 2),
+col3.metric(label='Case-Fatality Ratio \n(vs. US avg.)',
+            value=str(round(100*state_df['Mortality_Rate'][0], 2))+"%",
             delta=mort_rate_diff,
             delta_color='inverse')
-col4.metric(label='Incident Rate', value=int(state_df['Incident_Rate'][0]),
+col4.metric(label='Incident Rate \n(vs. US avg.)', value=int(state_df['Incident_Rate'][0]),
             delta=incident_rate_diff,
             delta_color='inverse')
